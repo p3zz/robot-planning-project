761b1e63d91fd53963a00c8c40c79a987ca912a1
p3zz <federico.pezzato.work@gmail.com>
2022-12-08 01:54:51 +0000

    add tests for segment-segment intersection

--- collisions/test/main.cpp
+++ collisions/test/main.cpp
@@ -1,7 +1,39 @@
 #include "gtest/gtest.h"
+#include "collisions/collisions.hpp"
 
-TEST(CollisionsTest, Segment){
-    EXPECT_TRUE(true);
+// incident
+TEST(CollisionsTest, SegmentIntersectSegmentIncident){
+    Segment s1(Point2D(0, 0), Point2D(2, 2));
+    Segment s2(Point2D(2, 0), Point2D(0, 2));
+    EXPECT_TRUE(s1.intersect(s2));
+}
+
+// not incident
+TEST(CollisionsTest, SegmentIntersectSegmenNotIncident){
+    Segment s1(Point2D(0, 0), Point2D(2, 10));
+    Segment s2(Point2D(1, 0), Point2D(3, 2));
+    EXPECT_FALSE(s1.intersect(s2));
+}
+
+// parallel
+TEST(CollisionsTest, SegmentIntersectSegmentParallel){
+    Segment s1(Point2D(0, 0), Point2D(2, 2));
+    Segment s2(Point2D(3, 0), Point2D(5, 2));
+    EXPECT_FALSE(s1.intersect(s2));
+}
+
+// parallel 2
+TEST(CollisionsTest, SegmentIntersectSegmentParallel2){
+    Segment s1(Point2D(0, 0), Point2D(2, 2));
+    Segment s2(Point2D(2, 0), Point2D(4, 2));
+    EXPECT_FALSE(s1.intersect(s2));
+}
+
+// overlap
+TEST(CollisionsTest, SegmentIntersectSegmentOverlap){
+    Segment s1(Point2D(0, 0), Point2D(2, 2));
+    Segment s2(Point2D(1, 1), Point2D(3, 3));
+    EXPECT_TRUE(s1.intersect(s2));
 }
 
 int main(){
