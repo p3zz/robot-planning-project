2882c1fdf5001565f25703609bec05629a79dd44
p3zz <federico.pezzato.work@gmail.com>
2022-12-09 11:49:46 +0000

    add Circle::get_angle() tests

--- collisions/test/circle.cpp
+++ collisions/test/circle.cpp
@@ -26,3 +26,19 @@ TEST(Circle, ContainsOutside){
     Point2D p(4,1);
     EXPECT_FALSE(c.contains(p));
 }
+
+// get_angle
+
+TEST(Circle, GetAngle1){
+    Circle c(Point2D(1, 1), 2);
+    Point2D p(2,2.73);
+    double angle = c.get_angle(p);
+    EXPECT_NEAR(angle, 1.0466, 0.01);
+}
+
+TEST(Circle, GetAngle2){
+    Circle c(Point2D(0, 0), 2);
+    Point2D p(-1.73, 1);
+    double angle = c.get_angle(p);
+    EXPECT_NEAR(angle, 2.61694, 0.01);
+}
