968926705de26dc2a39ba2f4edad25be0b729623
p3zz <federico.pezzato.work@gmail.com>
2022-12-10 14:47:25 +0000

    add polygon tests

---
+++ collisions/test/polygon.cpp
@@ -0,0 +1,23 @@
+#include "gtest/gtest.h"
+#include "collisions/collisions.hpp"
+
+// intersect segment
+
+TEST(Polygon, IntersectSegment1){
+    Polygon p({Point2D(1, 3), Point2D(2, 2), Point2D(2, 1), Point2D(-1, 1), Point2D(-2, 2), Point2D(-1, 4)});
+    Segment s(Point2D(-3, -1), Point2D(1, 4));
+    EXPECT_TRUE(p.intersect(s));
+}
+
+TEST(Polygon, IntersectSegment2){
+    Polygon p({Point2D(1, 3), Point2D(2, 2), Point2D(2, 1), Point2D(-1, 1), Point2D(-2, 2), Point2D(-1, 4)});
+    Segment s(Point2D(-3, -1), Point2D(3, -1));
+    EXPECT_FALSE(p.intersect(s));
+}
+
+TEST(Polygon, IntersectSegment3){
+    Polygon p({Point2D(1, 3), Point2D(2, 2), Point2D(2, 1), Point2D(-1, 1), Point2D(-2, 2), Point2D(-1, 4)});
+    Segment s(Point2D(0, 4), Point2D(4, 0));
+    EXPECT_TRUE(p.intersect(s));
+}
+
