0af44a653c356f34d3ae803c07bda43f63f44bd4
p3zz <federico.pezzato.work@gmail.com>
2022-12-27 12:54:51 +0000

    add map tests

---
+++ map/test/performance.cpp
@@ -0,0 +1,13 @@
+#include "gtest/gtest.h"
+#include "map/map.hpp"
+#include <chrono>
+#include <iostream>
+
+using std::chrono::high_resolution_clock;
+using std::chrono::duration_cast;
+using std::chrono::duration;
+using std::chrono::milliseconds;
+
+TEST(Map, Performance){
+    EXPECT_TRUE(true);
+}
