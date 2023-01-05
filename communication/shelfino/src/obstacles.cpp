75895a6560f9e38aa5a333a6ea4f9e6e61d12553
p3zz <federico.pezzato.work@gmail.com>
2023-01-05 13:50:32 +0000

    complete obstacles sumbscriber

--- communication/shelfino/src/obstacles.cpp
+++ communication/shelfino/src/obstacles.cpp
@@ -3,29 +3,38 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "custom_msgs/msg/obstacle_array_msg.hpp"
+#include "shapes/shapes.hpp"
 
 using std::placeholders::_1;
 
-class MinimalSubscriber : public rclcpp::Node
-{
+std::vector<Polygon> obstacles_from_msg(custom_msgs::msg::ObstacleArrayMsg msg){
+  std::vector<Polygon> obstacles;
+  for(auto obstacle: msg.obstacles){
+    Polygon p;
+    for(auto point: obstacle.polygon.points){
+      p.add_v(Point2D(point.x, point.y));
+    }
+    obstacles.push_back(p);
+  }
+  return obstacles;
+} 
+
+class MinimalSubscriber : public rclcpp::Node{
 public:
-  MinimalSubscriber()
-  : Node("obstacles_subscriber")
-  {
+  MinimalSubscriber() : Node("obstacles_subscriber") {
     subscription_ = this->create_subscription<custom_msgs::msg::ObstacleArrayMsg>(
       "obstacles", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
   }
 
 private:
-  void topic_callback(const custom_msgs::msg::ObstacleArrayMsg & msg) const
-  {
+  void topic_callback(const custom_msgs::msg::ObstacleArrayMsg & msg) const {
+    auto obstacles = obstacles_from_msg(msg);
     // RCLCPP_INFO_STREAM(this->get_logger(), "I heard: '" << msg.num << "'");
   }
   rclcpp::Subscription<custom_msgs::msg::ObstacleArrayMsg>::SharedPtr subscription_;
 };
 
-int main(int argc, char * argv[])
-{
+int main(int argc, char * argv[]) {
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<MinimalSubscriber>());
   rclcpp::shutdown();
