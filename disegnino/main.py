00841449cb14a6bfac241e8a6b464042a29f99f0
p3zz <federico.pezzato.work@gmail.com>
2023-01-21 14:24:22 +0100

    refactor main

--- disegnino/main.py
+++ disegnino/main.py
@@ -8,30 +8,47 @@ def tx(x):
 def ty(y):
     return hRcm-(y*100-offY)
 
-def main():
+def draw_obstacle(win, obstacle):
+    vertices = [Point(tx(v['x']), ty(v['y'])) for v in obstacle['inflated']]
+    c = Polygon(vertices)
+    c.setFill(color_rgb(255,168,168))
+    c.setOutline(color_rgb(255,168,168))
+    c.draw(win)
+    vertices = [Point(tx(v['x']), ty(v['y'])) for v in obstacle['normal']]
+    c = Polygon(vertices)
+    c.setFill("red")
+    c.draw(win)
 
-    ## Map & Roadmap ##
+def draw_exit(win, exit):
+    # draw regular exit
+    c = Circle(Point(tx(exit['normal']['x']), ty(exit['normal']['y'])), 8)
+    c.setFill('blue')
+    c.setOutline("blue")
+    c.draw(win)
+    # draw inflated exit
+    c = Circle(Point(tx(exit['inflated']['x']), ty(exit['inflated']['y'])), 8)
+    c.setFill('blue')
+    c.setOutline("blue")
+    c.draw(win)
+    # draw regular-inflated exit link
+    l = Line(Point(tx(exit['normal']['x']), ty(exit['normal']['y'])), Point(tx(exit['inflated']['x']), ty(exit['inflated']['y'])))
+    l.setWidth(1)
+    l.setFill('blue')
+    l.draw(win)
 
-    f = open('map.json')
-    data = json.load(f)
-    f.close()
+def draw_node(win, node):
+    c = Circle(Point(tx(node['x']), ty(node['y'])), 5)
+    c.setFill("black")
+    c.draw(win)
 
-    global offX, offY
-    offX=data['room']['min_x']*100
-    offY=data['room']['min_y']*100
-    global hRcm
-    hRcm=data['room']['h']*100
-    wRcm=data['room']['w']*100
-    vertexes=data['room']['vertexes']
-    vertexes_deflated=data['room']['vertexes_deflated']
-    win = GraphWin("roadmap", hRcm, wRcm)
+def draw_link(win, link):
+    node1 = Point(tx(link['node1']['x']), ty(link['node1']['y']))
+    node2 = Point(tx(link['node2']['x']), ty(link['node2']['y']))
+    c = Line(node1, node2)
+    c.setWidth(2)
+    c.draw(win)
 
-    roadmap = data['roadmap']
-    nodes = roadmap['nodes']
-    links = roadmap['links']
-    exits = roadmap['exits']
-    obstacles = roadmap['obstacles']
-
+def draw_room(win, vertexes, vertexes_deflated):
     vert = [Point(tx(v['x']), ty(v['y'])) for v in vertexes]
     c = Polygon(vert)
     c.setFill(color_rgb(255,168,168))
@@ -44,104 +61,98 @@ def main():
     c.setOutline("white")
     c.draw(win)
 
+def draw_map(win, data):
+    
+    # setup room borders
+    room_vertexes = data['room']['vertexes']
+    room_vertexes_deflated = data['room']['vertexes_deflated']
+    draw_room(win, room_vertexes, room_vertexes_deflated)
+
+    # setup roadmap
+    roadmap = data['roadmap']
+
+    # setup obstacles
+    obstacles = roadmap['obstacles']
     for obstacle in obstacles:
-        vertices = [Point(tx(v['x']), ty(v['y'])) for v in obstacle['inflated']]
-        c = Polygon(vertices)
-        c.setFill(color_rgb(255,168,168))
-        c.setOutline(color_rgb(255,168,168))
-        c.draw(win)
-        vertices = [Point(tx(v['x']), ty(v['y'])) for v in obstacle['normal']]
-        c = Polygon(vertices)
-        c.setFill("red")
-        c.draw(win)  
+        draw_obstacle(win, obstacle)  
 
+    # setup exits
+    exits = roadmap['exits']
     for ex in exits:
-        c = Circle(Point(tx(ex['normal']['x']), ty(ex['normal']['y'])), 8)
-        c.setFill('blue')
-        c.setOutline("blue")
-        c.draw(win)
-        c = Circle(Point(tx(ex['inflated']['x']), ty(ex['inflated']['y'])), 8)
-        c.setFill('blue')
-        c.setOutline("blue")
-        c.draw(win)
-        l = Line(Point(tx(ex['normal']['x']), ty(ex['normal']['y'])), Point(tx(ex['inflated']['x']), ty(ex['inflated']['y'])))
-        l.setWidth(1)
-        l.setFill('blue')
-        l.draw(win)
+        draw_exit(win, ex)
 
-    for point in nodes:
-        c = Circle(Point(tx(point['x']), ty(point['y'])), 5)
-        c.setFill("black")
-        c.draw(win)
+    # setup nodes
+    nodes = roadmap['nodes']
+    for node in nodes:
+        draw_node(win, node)
 
+    # setup links
+    links = roadmap['links']
     for link in links:
-        node1 = Point(tx(link['node1']['x']), ty(link['node1']['y']))
-        node2 = Point(tx(link['node2']['x']), ty(link['node2']['y']))
-        c = Line(node1, node2)
-        c.setWidth(2)
-        c.draw(win)
+        draw_link(win, link)
+
+def draw_moves(win, moves, color):
+    lines=[]
+    index=1
+    lst_x = tx(moves[0]["x"])
+    lst_y = ty(moves[0]["y"])
+    t = Text(Point(lst_x, lst_y-10), "0")
+    t.setSize(14)
+    t.setFill(color)
+    t.draw(win)
+    for move in moves[1:]:
+        act_x = tx(move["x"])
+        act_y = ty(move["y"])
+        l = Line(Point(lst_x, lst_y), Point(act_x, act_y))
+        l.setFill(color)
+        l.setWidth(2)
+        lines.append(l)
+        l.draw(win)
+        if(move["marked"]):
+            t = Text(Point(act_x, act_y-10), str(index))
+            t.setSize(14)
+            t.setFill(color)
+            t.draw(win)
+            index += 1
+        lst_x=act_x
+        lst_y=act_y
+
+
+def main():
+
+    if not exists('map.json'):
+        return
+
+    f = open('map.json')
+    data = json.load(f)
+    f.close()
+
+    # setup window
+    global offX, offY
+    offX = data['room']['min_x']*100
+    offY = data['room']['min_y']*100
+    global hRcm
+    hRcm = data['room']['h']*100
+    wRcm = data['room']['w']*100
+    win = GraphWin("roadmap", hRcm, wRcm)
+
+    # Map
+    draw_map(win, data)
 
     ## Moves ##
 
-    if exists('moves.json'):
-        f2 = open('moves.json')
-        data = json.load(f2)
-        f2.close()
-        moves_purser = data["moves_pursuer"]
-        moves_evader = data["moves_evader"]
-        lines=[]
-        
-        index=1
-        lst_x=tx(moves_purser[0]["x"])
-        lst_y=ty(moves_purser[0]["y"])
-        t=Text(Point(lst_x, lst_y-10), "0")
-        t.setSize(14)
-        t.setFill("green3")
-        t.draw(win)
-        for move in moves_purser[1:]:
-            act_x=tx(move["x"])
-            act_y=ty(move["y"])
-            l = Line(Point(lst_x, lst_y), Point(act_x, act_y))
-            l.setFill("green1")
-            l.setWidth(2)
-            lines.append(l)
-            #l.draw(win)
-            if(move["marked"]):
-                t=Text(Point(act_x, act_y-10), str(index))
-                t.setSize(14)
-                t.setFill("green3")
-                t.draw(win)
-                index+=1
-            lst_x=act_x
-            lst_y=act_y
+    if not exists('moves.json'):
+        return
 
-        index=1
-        lst_x=tx(moves_evader[0]["x"])
-        lst_y=ty(moves_evader[0]["y"])
-        t=Text(Point(lst_x, lst_y-10), "0")
-        t.setSize(14)
-        t.setFill("orange3")
-        t.draw(win)
-        for move in moves_evader[1:]:
-            act_x=tx(move["x"])
-            act_y=ty(move["y"])
-            l = Line(Point(lst_x, lst_y), Point(act_x, act_y))
-            l.setFill("orange")
-            l.setWidth(2)
-            lines.append(l)
-            #l.draw(win)
-            if(move["marked"]):
-                t=Text(Point(act_x, act_y-10), str(index))
-                t.setSize(14)
-                t.setFill("orange3")
-                t.draw(win)
-                index+=1
-            lst_x=act_x
-            lst_y=act_y
-        
-        for l in lines:
-            l.draw(win)
-            
+    f = open('moves.json')
+    data = json.load(f)
+    f.close()
+
+    moves_purser = data["moves_pursuer"]
+    moves_evader = data["moves_evader"]
+
+    draw_moves(win, moves_purser, "green")
+    draw_moves(win, moves_evader, "orange")    
         
     keyString = ""
     while keyString!="q":
